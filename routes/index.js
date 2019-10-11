00850f4bad42898df30d471b8739f1dffc1298f5
aidanbell0 <aidanbell0182@gmail.com>
2019-10-11 13:23:24 -0400

    Hook into API. Displaying Movie titles on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const request = require('request');
+const router = express.Router();
+
+const token = process.env.API_KEY;
+const rootURL = `https://api.themoviedb.org/3/person/2963/movie_credits?api_key=${token}&language=en-US`
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let list = null;
+  res.render('index', { list: list });
+});
+
+router.post('/', (req, res, next) => {
+  let options = {url: rootURL};
+  request(options, (err, response, body) => {
+    let list = JSON.parse(body);
+    res.render('index', {list: list});
+    console.log(list);
+  })
 });
 
 module.exports = router;
